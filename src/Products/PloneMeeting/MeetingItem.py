8bed2abd400a7ee907d631e3cab62de35e06f045
Gauthier Bastien <g.bastien@imio.be>
2012-08-24 09:15:19 +0000

    Force rich field content_type to text/html to be sure that we never have text/plain (sometime the case when the field is created empty...)

--- src/Products/PloneMeeting/MeetingItem.py
+++ src/Products/PloneMeeting/MeetingItem.py
@@ -2408,6 +2408,10 @@ class MeetingItem(OrderedBaseFolder, BrowserDefaultMixin):
         # Check if some copyGroups must be automatically added
         if self.isCopiesEnabled():
             self.addAutoCopyGroups()
+        # Make sure every Rich fields are 'text/html'
+        # sometimes (...) if empty, contentType is wrongly set to 'text/plain'...
+        for field in self.Schema().filterFields(default_content_type='text/html'):
+            field.setContentType(self, 'text/html')
         # Call sub-product-specific behaviour
         self.adapted().onEdit(isCreated=True)
         # Items that are created in the tool for creating recurring items
