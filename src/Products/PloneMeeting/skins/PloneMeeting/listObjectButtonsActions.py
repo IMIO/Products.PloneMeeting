ff4e8bbee56e36f141f91cc8a285f8f18770d08a
Gauthier Bastien <g.bastien@imio.be>
2012-10-15 08:15:57 +0000

    Get rid of deprecated actionicons

--- src/Products/PloneMeeting/skins/PloneMeeting/listObjectButtonsActions.py
+++ src/Products/PloneMeeting/skins/PloneMeeting/listObjectButtonsActions.py
@@ -20,13 +20,18 @@ res = []
 for action in objectButtonActions:
     if not (action['id'] in ignorableActions):
         act = [action['url']]
-        try:
-            # We try to append the url of the icon of the action
-            act.append(context.portal_actionicons.getActionIcon(
-                action['category'], action['id']))
-        except KeyError:
-            # Append nothing if no icon found
-            act.append('')
+        # We try to append the url of the icon of the action
+        # look on the action itself
+        if action['icon']:
+            act.append(action['icon'])
+        else:
+            # look for an icon in portal_actionicons, this is deprecated...
+            try:
+                act.append(context.portal_actionicons.getActionIcon(
+                    action['category'], action['id']))
+            except KeyError:
+                # Append nothing if no icon found
+                act.append('')
         act.append(action['title'])
         act.append(action['id'])
         res.append(act)
