e8bf4d3fd06d3a15db62ebccdfe6d39c897f165e
Gauthier Bastien <g.bastien@imio.be>
2012-10-22 08:29:24 +0000

    Prepare PM overlays also for meeting items lists.  See ticket #4846

--- src/Products/PloneMeeting/skins/PloneMeeting_styles/plonemeetingpopups.js
+++ src/Products/PloneMeeting/skins/PloneMeeting_styles/plonemeetingpopups.js
@@ -9,3 +9,20 @@ jQuery(function($){
    });
 
 });
+
+// this method is made to initialize overlays in the ajax-frame
+// because they are not correctly initialized at page load
+function initializePMOverlays(){
+jQuery(function($) {
+  
+  // Add advice popup
+  $('a.link-overlay').prepOverlay({
+     subtype: 'ajax',
+     urlmatch: '@@addeditadvice',
+  });
+});
+// as this method is called on the onmousover event of the ajax-frame
+// remove the event after first call to avoid it being called more than once
+tag = $('div#ajax-listitems-frame');
+tag[0].onmouseover = '';
+}
