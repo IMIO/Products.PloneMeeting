0621c304627d132aa3b744ad491996294e4c8d69
Gauthier Bastien <g.bastien@imio.be>
2012-10-29 08:13:41 +0000

    Use the main macro so if overlays are not initialized, it behave more or less correctly for the user...

--- src/Products/PloneMeeting/skins/plonemeeting_styles/plonemeetingpopups.js
+++ src/Products/PloneMeeting/skins/plonemeeting_styles/plonemeetingpopups.js
@@ -1,6 +1,7 @@
 /* The jQuery here above will load a jQuery popup */
 
 // prepare overlays for normal (non-ajax) pages
+// like meetingitem_view or overlays that you can raise from the portlet_plonemeeting
 jQuery(function($){
 
     // Add or edit advice popup
@@ -13,10 +14,12 @@ jQuery(function($){
 // prepare overlays in ajax frames
 // this method is made to initialize overlays in the ajax-frame
 // because they are not correctly initialized at page load
+// How does it work?  We add a handler "onmouseover" the produced div
+// containing the ajax-frame that only appear when the ajax-frame is loaded
+// "onmouseover" we initialize the overlays than remove the "onmouseover" event
+// so overlays are only initialized once...
 
 function initializePMOverlays(){
-
-
 jQuery(function($) {
   // Add advice popup
   $('a.link-overlay-pm.advice-overlay').prepOverlay({
@@ -34,18 +37,16 @@ jQuery(function($) {
 $('div.ajax-listitems-frame').each(function(){
     $(this).removeAttr('onmouseover');
     })
-
 }
 
-function initializePMOverlaysOnMeeting(){
-
-jQuery(function($) {
+// When on a meething view, we have to handle some more parameters to keep on wich page we are
 // Adapt the link that will show a popup for confirming a transition
 // to pass iStartNumber and lStartNumber that are global JS variables defined on the meeting_view
+function initializePMOverlaysOnMeeting(){
+jQuery(function($) {
 $('a.link-overlay-pm.transition-overlay').each(function(){
     $(this).attr('href',$(this).attr('href') + '&iStartNumber=' + iStartNumber + '&lStartNumber=' + lStartNumber);
     })
 })
 initializePMOverlays()
-
 }
