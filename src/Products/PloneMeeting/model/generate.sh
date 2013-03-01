9df2a474f878bbf5ffb3a9d35336031a3e5f588b
Gauthier Bastien <g.bastien@imio.be>
2013-03-01 11:04:47 +0000

    locales is now managed by imio.pm.locales so do not keep it when generating with AGX

--- src/Products/PloneMeeting/model/generate.sh
+++ src/Products/PloneMeeting/model/generate.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 /srv/archgenxml/agxtrunk/bin/archgenxml --cfg generate.conf PloneMeeting.zargo -o ..
+
+# remove the uselessly created 'locales' now managed by imio.pm.locales
+rm -rf ../locales
