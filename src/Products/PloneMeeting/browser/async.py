2add7ce3e30b798d81ca26720c275d10c7199058
Gauthier Bastien <g.bastien@imio.be>
2013-06-19 12:28:05 +0000

    Removed meetingitem_discuss python script that fails now with new hotfix (see #6236).  As we were refactoring, made the synchronous toggle manage the fact of 'asking' an item to be discussed by reviewers (see #4202)

--- src/Products/PloneMeeting/browser/async.py
+++ src/Products/PloneMeeting/browser/async.py
@@ -13,17 +13,28 @@ class Discuss(BrowserView):
         portal_state = getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
         self.portal = portal_state.portal()
 
-    def available(self):
-        return False
+    def isAsynchToggleEnabled(self):
+        """
+          Return True if the asynchronous call is enabled.
+          Indeed, in some case, it can be necessary to deactivate to asynchronous call
+          so the page is reloaded entirely if some other area are updated after
+          having toggled the toDiscuss value.
+        """
+        return True
 
-    def toggle(self):
-
+    def asynchToggle(self):
+        """
+          Toggle the MeetingItem.toDiscuss attribute from True to False and False to True
+          asynchronously, meaning that the entire page is not fully reloaded but only the
+          relevant icon.
+        """
         member = self.portal.restrictedTraverse('@@plone_portal_state').member()
         if not member.has_permission('Modify portal content', self.context):
             raise Unauthorized
 
         toDiscuss = not self.context.getToDiscuss()
         self.context.setToDiscuss(toDiscuss)
+        self.context.adapted().onDiscussChanged(toDiscuss)
 
         if toDiscuss:
             filename = 'toDiscussYes.png'
@@ -44,6 +55,34 @@ class Discuss(BrowserView):
 
         html = self.IMG_TEMPLATE % (src, title, name)
         return html
+
+    def synchToggle(self, itemUid, discussAction):
+        """
+          This is a synchronous way of toggling toDiscuss.
+          The asynchronous asynchToggle here above will only reload the clicked icon.
+          If for some reason it is necessary that the page is fully reloaded,
+          like for example to display a portal_message or because something else
+          has changed on the page, this is the method to use.
+          Here, it manages for example the fact that a reviewer can ask
+          an item to be discussed and that will display a portal_message to this user.
+        """
+        item = self.context.uid_catalog(UID=itemUid)[0].getObject()
+        if discussAction == 'ask':
+            # I must send a mail to MeetingManagers for notifying them that a reviewer
+            # wants to discuss this item.
+            sendMailEnabled = item.sendMailIfRelevant('askDiscussItem', 'MeetingManager', isRole=True)
+            if sendMailEnabled:
+                msgId = 'to_discuss_ask_mail_sent'
+            else:
+                msgId = 'to_discuss_ask_mail_not_sent'
+            self.context.plone_utils.addPortalMessage(
+                item.translate(msgId, domain='PloneMeeting'))
+        elif discussAction == 'toggle':
+            # I must toggle the "toDiscuss" switch on the item
+            toDiscuss = not item.getToDiscuss()
+            item.setToDiscuss(toDiscuss)
+            item.adapted().onDiscussChanged(toDiscuss)
+        return self.context.portal_plonemeeting.gotoReferer()
 
 
 class AnnexToPrint(BrowserView):
