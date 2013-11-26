cf77e7d548031d3502cc58095347776b07c2e39e
Gauthier Bastien <g.bastien@imio.be>
2013-11-26 15:12:50 +0000

    Moved necessary elements to imio.actionspanel

--- src/Products/PloneMeeting/skins/plonemeeting_styles/plonemeetingpopups.js
+++ src/Products/PloneMeeting/skins/plonemeeting_styles/plonemeetingpopups.js
@@ -44,7 +44,7 @@ jQuery(function($) {
 });
 jQuery(function($) {
   // Add transition confirmation popup
-  $('a.link-overlay-pm.transition-overlay').prepOverlay({
+  $('a.link-overlay-actionspanel.transition-overlay').prepOverlay({
         subtype: 'ajax',
         closeselector: '[name="form.buttons.cancel"]',
   });
