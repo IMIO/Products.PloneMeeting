1d2840698bb745977dc141bb18f2297ef6890266
Gauthier Bastien <g.bastien@imio.be>
2013-12-10 16:28:55 +0000

    Added migration step for new advice type

--- src/Products/PloneMeeting/migrations/migrate_to_3_1_1.py
+++ src/Products/PloneMeeting/migrations/migrate_to_3_1_1.py
@@ -1,6 +1,9 @@
 # ------------------------------------------------------------------------------
 import logging
 logger = logging.getLogger('PloneMeeting')
+from Acquisition import aq_base
+from plone.app.textfield.value import RichTextValue
+from plone.dexterity.utils import createContentInContainer
 from Products.PloneMeeting.migrations import Migrator
 
 
@@ -28,10 +31,48 @@ class Migrate_To_3_1_1(Migrator):
             field.setContentType(cfg, 'text/html')
         logger.info('Done.')
 
+    def _updateAdvices(self):
+        '''We do not have a MeetingItem.advices anymore, we use MeetingItem.adviceIndex
+           and meetingadvice objects.'''
+        brains = self.portal.portal_catalog(meta_type=('MeetingItem', ))
+        logger.info('Updating advices for %d MeetingItem objects...' % len(brains))
+        for brain in brains:
+            item = brain.getObject()
+            if hasattr(aq_base(item), 'advices'):
+                for groupId, advice in aq_base(item).advices.iteritems():
+                    if advice['type'] != 'not_given':
+                        advice_comment = advice['comment']
+                        if not isinstance(advice['comment'], unicode):
+                            advice_comment = unicode(advice_comment, 'utf-8')
+                        meetingadvice = createContentInContainer(item,
+                                                                 'meetingadvice',
+                                                                 **{'advice_group': groupId,
+                                                                 'advice_type': advice['type'],
+                                                                 'advice_comment': RichTextValue(advice_comment)})
+                        meetingadvice.setCreators((advice['actor'], ))
+                        meetingadvice.setCreationDate(advice['date'])
+                delattr(item, 'advices')
+                # Update security as local_roles are set by updateAdvices
+                item.reindexObject()
+        logger.info('Updating advices for items of MeetingConfigs...')
+        for cfg in self.portal.portal_plonemeeting.objectValues('MeetingConfig'):
+            for item in cfg.recurringitems.objectValues('MeetingItem'):
+                if hasattr(aq_base(item), 'advices'):
+                    for groupId, advice in aq_base(item).advices.iteritems():
+                        createContentInContainer(item,
+                                                 'meetingadvice',
+                                                 **{'advice_group': groupId,
+                                                    'advice_type': advice['type'],
+                                                    'advice_comment': RichTextValue(advice['comment'])})
+                    delattr(item, 'advices')
+                    item.updateAdvices()
+        logger.info('Done.')
+
     def run(self):
         logger.info('Migrating to PloneMeeting 3.1.1...')
         self._configureCatalogIndexesAndMetadata()
         self._initDefaultBudgetHTML()
+        self._updateAdvices()
         # reinstall so 'getDeliberation' index is added and computed
         self.reinstall(profiles=[u'profile-Products.PloneMeeting:default', ])
         self.finish()
@@ -43,7 +84,8 @@ def migrate(context):
 
        1) Removed the 'getDecision' index;
        2) Initialize field MeetingConfig.defaultBudget so it behaves correctly has RichText;
-       3) Reinstall PloneMeeting so new index 'getDeliberation' is added and computed.
+       3) Update advices as we moved from MeetingItem.advices to MeetingItem.adviceIndex;
+       4) Reinstall PloneMeeting so new index 'getDeliberation' is added and computed.
     '''
     Migrate_To_3_1_1(context).run()
 # ------------------------------------------------------------------------------
