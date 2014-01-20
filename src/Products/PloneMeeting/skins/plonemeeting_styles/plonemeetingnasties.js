a53da2d580d1357e1ef997a69ee7eb21649b6269
Gauthier Bastien <g.bastien@imio.be>
2014-01-20 08:45:21 +0000

    Disable 'not_selectable_...' options from the optionalAdvisers select box

--- src/Products/PloneMeeting/skins/plonemeeting_styles/plonemeetingnasties.js
+++ src/Products/PloneMeeting/skins/plonemeeting_styles/plonemeetingnasties.js
@@ -4,4 +4,20 @@ removeLinkOnByLine = function() {
     $(tagToHandle).replaceWith('<span>' + $(tagToHandle).html() + '</span>');
 }
 
+/* Disable some elements from the optionalAdvisers list box
+   Actually we add 2 special values that are help messages related to delay-aware advisers,
+   these shoul not be selectable. */
+disableSomeOptionsFromOptionalAdvisers = function() {
+    box = $('select#optionalAdvisers option');
+    box.each(function() {
+        var valuesToDisable = ['not_selectable_value_delay_aware_optional_advisers',
+                               'not_selectable_value_non_delay_aware_optional_advisers'];
+
+        if ($.inArray($(this).val(), valuesToDisable) >= 0) {
+            $(this).attr("disabled", "disabled");
+        }
+    });
+}
+
 jQuery(document).ready(removeLinkOnByLine);
+jQuery(document).ready(disableSomeOptionsFromOptionalAdvisers);
