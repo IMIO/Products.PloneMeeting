9b63814e3241930569da36dc00734447e874b122
Gauthier Bastien <g.bastien@imio.be>
2014-03-12 13:33:07 +0000

    Added monkey patch for workflow.xml GS import so attribute 'purge' is taken into account.  See ticket #7692

--- src/Products/PloneMeeting/monkey.py
+++ src/Products/PloneMeeting/monkey.py
@@ -1,1 +1,24 @@
-# nothing for now ;-)
+from Products.CMFCore.exportimport.workflow import WorkflowToolXMLAdapter
+
+
+def _importNode(self, node):
+    """Import the object from the DOM node.
+    """
+    # XXX --- Begin change by PM ---
+    purge = self.environ.shouldPurge()
+    if node.getAttribute('purge'):
+        purge = self._convertToBoolean(node.getAttribute('purge'))
+    if purge:
+    #if self.environ.shouldPurge():
+    # XXX --- End change by PM ---
+        self._purgeProperties()
+        self._purgeObjects()
+        self._purgeChains()
+
+    self._initProperties(node)
+    self._initObjects(node)
+    self._initChains(node)
+
+    self._logger.info('Workflow tool imported.')
+
+WorkflowToolXMLAdapter._importNode = _importNode
