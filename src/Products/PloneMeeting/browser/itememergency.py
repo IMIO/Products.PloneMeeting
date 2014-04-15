64b6f7c1ce820c3b14cd631cb00e7745f3343153
Gauthier Bastien <g.bastien@imio.be>
2014-04-15 11:51:12 +0000

    Continue work about MeetingItem.emergency, see ticket #7384

--- src/Products/PloneMeeting/browser/itememergency.py
+++ src/Products/PloneMeeting/browser/itememergency.py
@@ -1,6 +1,54 @@
 from AccessControl import Unauthorized
 from DateTime import DateTime
+from Products.Archetypes import DisplayList
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
+
+
+class ItemEmergencyView(BrowserView):
+    '''Render the item emergency HTML on the meetingitem_view.'''
+
+    def __init__(self, context, request):
+        super(BrowserView, self).__init__(context, request)
+        self.context = context
+        self.request = request
+        self.portal_url = getToolByName(self, 'portal_url').getPortalObject().absolute_url()
+
+    def listSelectableEmergencies(self):
+        '''Returns a list of emergencies the current user can set the item to.'''
+        # we base our actions on avaialble terms in the MeetingItem.emergency field vocabulary
+        emergencies = self.context.listEmergencies()
+        emergencyKeys = emergencies.keys()
+        currentEmergency = self.context.getEmergency()
+        # now check if user can ask emergency if it is not already the case
+        if not self.mayAskEmergency():
+            emergencyKeys.remove('emergency_asked')
+        # now check if user can accept/refuse and asked emergency if it is not already the case
+        if not self.mayAcceptOrRefuseEmergency():
+            emergencyKeys.remove('emergency_accepted')
+            emergencyKeys.remove('emergency_refused')
+        # now if currentEmergency is still in emergencies, we remove it
+        if currentEmergency in emergencies:
+            emergencyKeys.remove(currentEmergency)
+        # now build a vocabulary with left values
+        res = []
+        for emergency in emergencies.items():
+            if emergency[0] in emergencyKeys:
+                res.append(emergency)
+        return DisplayList(tuple(res))
+
+    def mayAskEmergency(self):
+        '''Returns True if current user may ask emergency for an item.'''
+        # by default, everybody able to edit the item can ask emergency
+        return True
+
+    def mayAcceptOrRefuseEmergency(self):
+        '''Returns True if current user may accept or refuse emergency if asked for an item.'''
+        # by default, only MeetingManagers can accept or refuse emergency
+        tool = getToolByName(self, 'portal_plonemeeting')
+        if tool.isManager():
+            return True
+        return False
 
 
 class ChangeItemEmergencyView(BrowserView):
