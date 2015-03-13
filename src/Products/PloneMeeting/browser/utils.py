addb98478dad26a3400408cd099c7e529dd6c143
Gauthier Bastien <g.bastien@imio.be>
2015-03-13 14:28:25 +0000

    Make sure cropped advice comment does not break page layout as it is HTML...  See ticket #9425

---
+++ src/Products/PloneMeeting/browser/utils.py
@@ -0,0 +1,19 @@
+from BeautifulSoup import BeautifulSoup
+from zope.component import getMultiAdapter
+
+from Products.Five import BrowserView
+
+
+class Utils(BrowserView):
+    """
+      This manage to let some utilities available
+    """
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        portal_state = getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
+        self.portal = portal_state.portal()
+
+    def cropHTML(self, html, length):
+        '''Crop given HTML and return valid HTML.'''
+        return BeautifulSoup(html[:length]).renderContents()
