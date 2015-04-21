84b96c33672bd7a46953a55b91335d849bde0f62
Gauthier Bastien <g.bastien@imio.be>
2015-04-21 09:10:15 +0000

    Removed override of archetypes.referencebrowserwidget popup now that we use version 2.5.3 that manage wildcard search, this close ticket #10549

--- src/Products/PloneMeeting/browser/overrides.py
+++ src/Products/PloneMeeting/browser/overrides.py
@@ -7,9 +7,7 @@
 # GNU General Public License (GPL)
 #
 
-from Acquisition import aq_base
 from zope.annotation import IAnnotations
-from archetypes.referencebrowserwidget.browser.view import ReferenceBrowserPopup
 from plone.app.controlpanel.overview import OverviewControlPanel
 from plone.app.layout.viewlets.common import ContentActionsViewlet
 from plone.app.layout.viewlets.common import GlobalSectionsViewlet
@@ -25,7 +23,6 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.permissions import ModifyPortalContent
 from Products.PloneMeeting.utils import getCurrentMeetingObject
-from Products.PloneMeeting.utils import prepareSearchValue
 
 
 class PloneMeetingGlobalSectionsViewlet(GlobalSectionsViewlet):
@@ -121,24 +118,6 @@ class PloneMeetingOverviewControlPanel(OverviewControlPanel):
         pm_version = self.context.portal_setup.getProfileInfo('profile-Products.PloneMeeting:default')['version']
         versions.insert(0, 'PloneMeeting %s' % pm_version)
         return versions
-
-
-class PMReferenceBrowserPopup(ReferenceBrowserPopup):
-    '''Overrides so searches using SearchableText are done wildcard.
-       Moreover, we display the item type in the shown title.'''
-
-    def getResult(self):
-        assert self._updated
-        search_value = self.request.get('SearchableText', '')
-        if search_value:
-            self.request.set('SearchableText',
-                             prepareSearchValue(search_value))
-        return super(PMReferenceBrowserPopup, self).getResult()
-
-    def title_or_id(self, item):
-        assert self._updated
-        item = aq_base(item)
-        return getattr(item, 'title_or_id')
 
 
 class BaseActionsPanelView(ActionsPanelView):
