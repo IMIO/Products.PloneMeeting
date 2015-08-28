1eaf6e4345f37ee668d111e8eca3dbedfd53228b
Gauthier Bastien <g.bastien@imio.be>
2015-08-28 14:44:57 +0000

    Corrected test and finished it...

--- src/Products/PloneMeeting/tests/testPortlets.py
+++ src/Products/PloneMeeting/tests/testPortlets.py
@@ -30,17 +30,28 @@ class testPortlets(PloneMeetingTestCase):
 
     def test_pm_PortletPMAvailableTemplates(self):
         '''Test the portlet_plonemeeting itemTemplates icon that is shown if item templates
-           are defined in the configuration.'''
+           are defined in the configuration, no matter current user have item templates or not,
+           this way we have something coherent between users, even for users without itemTemplates.'''
+        # remove every templates and add one restricted to 'developers'
+        self.changeUser('siteadmin')
+        self._removeConfigObjectsFor(self.meetingConfig, folders=['recurringitems', 'itemtemplates'])
+        itemTemplate = self.create('MeetingItemTemplate')
+        itemTemplate.setTemplateUsingGroups(('developers', ))
+        itemTemplate.reindexObject(idxs=['templateUsingGroups', ])
         # pmCreator1 is member of 'developers'
         self.changeUser('pmCreator1')
         pmFolder1 = self.getMeetingFolder()
         itemsCategory = pmFolder1.restrictedTraverse('@@render_collection_widget_category')
-        itemsCategory(category=('', ''),  widget=None)
+        itemsCategory(widget=None)
         self.assertTrue(itemsCategory.templateItems())
         # pmCreator2 is member of 'vendors'
+        self.changeUser('pmCreator2')
         pmFolder2 = self.getMeetingFolder()
         itemsCategory = pmFolder2.restrictedTraverse('@@render_collection_widget_category')
-        itemsCategory(category=('', ''),  widget=None)
+        itemsCategory(widget=None)
+        self.assertTrue(itemsCategory.templateItems())
+        # no matter actually there are no itemTemplates available for him...
+        self.assertFalse(self.meetingConfig.getItemTemplates(as_brains=True, onlyActive=True, filtered=True))
 
     def test_pm_CreateItemFromTemplate(self):
         '''
