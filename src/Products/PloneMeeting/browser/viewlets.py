698875fb8cf4b9c757bc8d3bff59371cedbc0147
Gauthier Bastien <g.bastien@imio.be>
2015-10-29 13:04:36 +0000

    Removed useless pod_templates old viewlet, we use the one from collective.documentgenerator now

--- src/Products/PloneMeeting/browser/viewlets.py
+++ src/Products/PloneMeeting/browser/viewlets.py
@@ -1,36 +1,29 @@
-# ------------------------------------------------------------------------------
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2015 by Imio.be
+#
+# GNU General Public License (GPL)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
 from plone.app.layout.viewlets import ViewletBase
 from zope.component import getMultiAdapter
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
-
-
-# ------------------------------------------------------------------------------
-class PodTemplatesViewlet(ViewletBase):
-    '''This viewlet displays the available pod templates for an item or a
-       meeting.'''
-
-    def update(self):
-        self.context_state = getMultiAdapter((self.context, self.request),
-                                             name=u'plone_context_state')
-
-    def getCurrentMeetingConfig(self):
-        '''Returns the current meetingConfig.'''
-        portal_plonemeeting = getToolByName(self.context, 'portal_plonemeeting')
-        return portal_plonemeeting.getMeetingConfig(self.context)
-
-    def getPloneMeetingTool(self):
-        '''.Returns portal_plonemeeting.'''
-        return getToolByName(self.context, 'portal_plonemeeting')
-
-    def getCurrentObject(self):
-        '''Returns the current object.'''
-        return self.context
-
-    def getPortalUrl(self):
-        return getToolByName(self.context, 'portal_url').getPortalPath()
-
-    index = ViewPageTemplateFile("templates/pod_templates.pt")
 
 
 class WorkflowState(ViewletBase):
