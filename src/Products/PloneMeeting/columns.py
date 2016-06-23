2dece7a7d453fae5367bae63c4b4a7e0315911fa
Gauthier Bastien <g.bastien@imio.be>
2016-06-23 09:39:05 +0000

    Use right attr to get group in charge

--- src/Products/PloneMeeting/columns.py
+++ src/Products/PloneMeeting/columns.py
@@ -39,14 +39,14 @@ class ItemProposingGroupAcronymColumn(AbbrColumn):
 class ItemGroupInChargeColumn(VocabularyColumn):
     """A column that display the proposingGroup.groupInCharge
     taking into account meeting date."""
-    attrName = 'getProposingGroupInCharge'
+    attrName = 'getGroupInCharge'
     vocabulary = u'Products.PloneMeeting.vocabularies.proposinggroupsvocabulary'
 
 
 class ItemGroupInChargeAcronymColumn(AbbrColumn):
     """A column that display the proposingGroup.groupInCharge
     acronym taking into account meeting date."""
-    attrName = 'getProposingGroupInCharge'
+    attrName = 'getGroupInCharge'
     vocabulary = u'Products.PloneMeeting.vocabularies.proposinggroupacronymsvocabulary'
     full_vocabulary = u'Products.PloneMeeting.vocabularies.proposinggroupsvocabulary'
 
