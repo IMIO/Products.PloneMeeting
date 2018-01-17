abcfc24467b5bc5501f607abe23eaa7647bc5d8a
Gauthier Bastien <g.bastien@imio.be>
2018-01-17 13:48:00 +0000

    Merged back last changes from branch 4.0

--- src/Products/PloneMeeting/tests/testMeeting.py
+++ src/Products/PloneMeeting/tests/testMeeting.py
@@ -2064,6 +2064,7 @@ class testMeeting(PloneMeetingTestCase):
     def test_pm_GetNextMeeting(self):
         """Test the getNextMeeting method that will return the next meeting
            regarding the meeting date."""
+        cfg2Id = self.meetingConfig2.getId()
         self.changeUser('pmManager')
         meeting = self.create('Meeting', date=DateTime('2015/01/15'))
         # no next meeting for now
@@ -2075,13 +2076,14 @@ class testMeeting(PloneMeetingTestCase):
         self.assertEquals(meeting2.getNextMeeting(), meeting3)
         self.assertFalse(meeting3.getNextMeeting())
 
-        self.assertFalse(meeting.getNextMeeting(cfgId=self.meetingConfig2.getId()))
-        otherMCMeeting = self.create('Meeting', date=DateTime('2015/01/20'), meetingConfig=self.meetingConfig2)
-        otherMCMeeting2 = self.create('Meeting', date=DateTime('2015/01/25'), meetingConfig=self.meetingConfig2)
-        self.assertEqual(meeting.getNextMeeting(cfgId=self.meetingConfig2.getId()), otherMCMeeting)
+        self.assertFalse(meeting.getNextMeeting(cfgId=cfg2Id))
+        self.setMeetingConfig(cfg2Id)
+        otherMCMeeting = self.create('Meeting', date=DateTime('2015/01/20'))
+        otherMCMeeting2 = self.create('Meeting', date=DateTime('2015/01/25'))
+        self.assertEqual(meeting.getNextMeeting(cfgId=cfg2Id), otherMCMeeting)
         # with the date gap, the meeting 5 days later is not taken into account.
-        self.assertNotEqual(meeting.getNextMeeting(cfgId=self.meetingConfig2.getId(), dateGap=7), otherMCMeeting)
-        self.assertEqual(meeting.getNextMeeting(cfgId=self.meetingConfig2.getId(), dateGap=7), otherMCMeeting2)
+        self.assertNotEqual(meeting.getNextMeeting(cfgId=cfg2Id, dateGap=7), otherMCMeeting)
+        self.assertEqual(meeting.getNextMeeting(cfgId=cfg2Id, dateGap=7), otherMCMeeting2)
 
     def test_pm_GetPreviousMeeting(self):
         """Test the getPreviousMeeting method that will return the previous meeting
