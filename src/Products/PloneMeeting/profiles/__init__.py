10b53ea1a8f3415f01eeafb135fb9570074b0cb2
Gauthier Bastien <g.bastien@imio.be>
2018-12-14 14:02:46 +0000

    Do not enable filter 'Taken over by' by default

--- src/Products/PloneMeeting/profiles/__init__.py
+++ src/Products/PloneMeeting/profiles/__init__.py
@@ -642,7 +642,7 @@ class MeetingConfigDescriptor(Descriptor):
         self.toDoListSearches = []
         # advanced filters shown
         self.dashboardItemsListingsFilters = ('c4', 'c6', 'c7', 'c8', 'c9', 'c10',
-                                              'c11', 'c12', 'c13', 'c14', 'c15', 'c16')
+                                              'c11', 'c13', 'c14', 'c15', 'c16')
         self.dashboardMeetingAvailableItemsFilters = ('c4', 'c11', 'c16')
         self.dashboardMeetingLinkedItemsFilters = ('c4', 'c6', 'c7', 'c11', 'c12', 'c16', 'c19')
         self.groupsHiddenInDashboardFilter = []
