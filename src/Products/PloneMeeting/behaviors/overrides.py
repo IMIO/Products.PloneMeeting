92ae98431c14909a5b702c5e4bc4d86c2cc6ba44
Gauthier Bastien <g.bastien@imio.be>
2019-03-08 20:33:01 +0100

    Fixed .gitignore that ignored new folder behaviors/

---
+++ src/Products/PloneMeeting/behaviors/overrides.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# File: adapters.py
+#
+# Copyright (c) 2019 by Imio.be
+#
+# GNU General Public License (GPL)
+#
+
+
+from collective.behavior.talcondition.behavior import TALCondition
+from plone import api
+
+
+class PMTALCondition(TALCondition):
+    """Override TALCondition behavior to add our own TAL expression extra context."""
+
+    def complete_extra_expr_ctx(self, extra_expr_ctx):
+        """Return extra_expr_ctx, this is made to be overrided."""
+        extra_context = super(PMTALCondition, self).complete_extra_expr_ctx(extra_expr_ctx)
+        tool = api.portal.get_tool('portal_plonemeeting')
+        context = extra_context.get('context', self.context)
+        cfg = tool.getMeetingConfig(context)
+        extra_context.update({'tool': tool,
+                              'cfg': cfg})
+        return extra_context
