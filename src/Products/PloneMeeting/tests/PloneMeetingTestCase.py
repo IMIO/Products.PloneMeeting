b6acdef8247e9335a02b91d24017022ec3e2c9fc
Gauthier Bastien <g.bastien@imio.be>
2019-06-04 15:21:15 +0200

    Doc

--- src/Products/PloneMeeting/tests/PloneMeetingTestCase.py
+++ src/Products/PloneMeeting/tests/PloneMeetingTestCase.py
@@ -148,6 +148,7 @@ class PloneMeetingTestCase(unittest.TestCase, PloneMeetingTestingHelpers):
         self.annexFileTypeDecision = 'decision-annex'
         self.annexFileTypeAdvice = 'advice-annex'
         self.annexFileTypeMeeting = 'meeting-annex'
+        # log current test name
         pm_logger.info('Executing {0}'.format(self._testMethodName))
 
     def tearDown(self):
