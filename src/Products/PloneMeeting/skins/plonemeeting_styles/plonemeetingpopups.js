5b9eaf779f17f5f4bc789de93c50cf6057316832
Gauthier Bastien <g.bastien@imio.be>
2019-09-02 16:31:47 +0200

    Use default trigger options when possible

--- src/Products/PloneMeeting/skins/plonemeeting_styles/plonemeetingpopups.js
+++ src/Products/PloneMeeting/skins/plonemeeting_styles/plonemeetingpopups.js
@@ -282,16 +282,10 @@ function advicesInfos() {
       data_parameters=['adviceType'],
       options={zIndex: 5000,
                position: 'left',
-               trigger: 'custom',
-               triggerOpen: {
-                  mouseenter: true,
-                  click: true,
-                  tap: true,
-                  },
+                // do not close on mouseleave
                triggerClose: {
                   click: true,
-                  tap: true,
-                  },
+                  tap: true, },
                functionReady_callback: initializeAdvicePopup,
                minWidth: 700,
                maxWidth: 700,
@@ -302,16 +296,10 @@ function advicesInfos() {
       data_parameters=['adviceType'],
       options={zIndex: 1,
                position: 'bottom',
-               trigger: 'custom',
-               triggerOpen: {
-                  mouseenter: true,
-                  click: true,
-                  tap: true,
-                  },
+                // do not close on mouseleave
                triggerClose: {
                   click: true,
-                  tap: true,
-                  },
+                  tap: true, },
                functionReady_callback: initializeAdvicePopup,
                minWidth: 700,
                maxWidth: 700,
