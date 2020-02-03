0217a6e2abbe85d8834bcb3b1507acf2e23f079c
Gauthier Bastien <g.bastien@imio.be>
2020-02-03 20:51:12 +0100

    Fixed test_pm_WFA_availableWFAdaptations, consider new WFA 'waiting_advices_from_before_last_val_level'

--- src/Products/PloneMeeting/tests/testWFAdaptations.py
+++ src/Products/PloneMeeting/tests/testWFAdaptations.py
@@ -49,6 +49,7 @@ class testWFAdaptations(PloneMeetingTestCase):
                           'return_to_proposing_group_with_all_validations',
                           'return_to_proposing_group_with_last_validation',
                           'reviewers_take_back_validated_item',
+                          'waiting_advices_from_before_last_val_level',
                           'waiting_advices_from_last_val_level_adviser_and_proposing_group_send_back',
                           'waiting_advices_from_last_val_level_only_adviser_send_back',
                           'waiting_advices_from_last_val_level_only_proposing_group_send_back'])
