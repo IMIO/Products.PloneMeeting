9c3655d8d0daa3e77ddf29934310fda91ebbeb4b
Gauthier Bastien <g.bastien@imio.be>
2020-05-25 16:01:43 +0200

    Fixed test_pm_SearchItemsToValidateOfMyReviewerGroups using ram.cache invalidation key smart enough

--- src/Products/PloneMeeting/monkey.py
+++ src/Products/PloneMeeting/monkey.py
@@ -1,15 +1,16 @@
 
 from Acquisition import aq_base
 from imio.annex.content.annex import Annex
+from imio.helpers.cache import get_cachekey_volatile
 from plone import api
 from plone.api.exc import InvalidParameterError
 from plone.app.querystring import queryparser
+from plone.memoize import ram
 from Products.Archetypes.BaseObject import BaseObject
 from Products.CMFPlone.CatalogTool import CatalogTool
 from Products.PloneMeeting import logger
 from Products.PloneMeeting.content.advice import MeetingAdvice
 from Products.PortalTransforms.cache import Cache
-from zope.annotation import IAnnotations
 
 
 def _patched_equal(context, row):
@@ -85,40 +86,43 @@ BaseObject.validate = validate
 logger.info("Monkey patching Products.Archetypes.BaseObject.BaseObject (validate)")
 
 
+def _listAllowedRolesAndUsers_cachekey(method, self, user):
+    '''cachekey method for self._listAllowedRolesAndUsers.'''
+    date = get_cachekey_volatile('Products.PloneMeeting.ToolPloneMeeting.get_plone_groups_for_user')
+    try:
+        tool = api.portal.get_tool('portal_plonemeeting')
+        users_groups = tool._users_groups_value()
+    except InvalidParameterError:
+        users_groups = None
+    return date, users_groups, user.getId()
+
+
+@ram.cache(_listAllowedRolesAndUsers_cachekey)
 def _listAllowedRolesAndUsers(self, user):
     """Monkeypatch to use ToolPloneMeeting.get_plone_groups_for_user instead getGroups.
        Moreover store this in the REQUEST."""
-    data = None
-    key = "catalog-listAllowedRolesAndUsers-{0}".format(user.getId())
-    # async does not have a REQUEST
-    if hasattr(self, 'REQUEST'):
-        cache = IAnnotations(self.REQUEST)
-        data = cache.get(key, None)
 
-    if data is None:
-        # Makes sure the list includes the user's groups.
-        result = user.getRoles()
-        if 'Anonymous' in result:
-            # The anonymous user has no further roles
-            return ['Anonymous']
-        result = list(result)
-        # XXX change, replaced getGroups by tool.get_plone_groups_for_user
-        # if hasattr(aq_base(user), 'getGroups'):
-        #     groups = ['user:%s' % x for x in user.getGroups()]
-        try:
-            tool = api.portal.get_tool('portal_plonemeeting')
-            groups = tool.get_plone_groups_for_user()
-        except InvalidParameterError:
-            groups = user.getGroups()
-        if groups:
-            groups = ['user:%s' % x for x in groups]
-            result = result + groups
-        # Order the arguments from small to large sets
-        result.insert(0, 'user:%s' % user.getId())
-        result.append('Anonymous')
-        data = result
-        cache[key] = data
-    return data
+    # Makes sure the list includes the user's groups.
+    result = user.getRoles()
+    if 'Anonymous' in result:
+        # The anonymous user has no further roles
+        return ['Anonymous']
+    result = list(result)
+    # XXX change, replaced getGroups by tool.get_plone_groups_for_user
+    # if hasattr(aq_base(user), 'getGroups'):
+    #     groups = ['user:%s' % x for x in user.getGroups()]
+    try:
+        tool = api.portal.get_tool('portal_plonemeeting')
+        groups = tool.get_plone_groups_for_user()
+    except InvalidParameterError:
+        groups = user.getGroups()
+    if groups:
+        groups = ['user:%s' % x for x in groups]
+        result = result + groups
+    # Order the arguments from small to large sets
+    result.insert(0, 'user:%s' % user.getId())
+    result.append('Anonymous')
+    return result
 
 CatalogTool._listAllowedRolesAndUsers = _listAllowedRolesAndUsers
 logger.info("Monkey patching Products.CMFPlone.CatalogTool.CatalogTool (_listAllowedRolesAndUsers)")
