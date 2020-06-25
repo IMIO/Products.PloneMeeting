7a91efeac22354ef096fab28d48d2a3419f3f9c3
Gauthier Bastien <g.bastien@imio.be>
2020-06-25 18:26:52 +0200

    Only change email_from_address if smtp_host is localhost

--- src/Products/PloneMeeting/migrations/migrate_to_4109.py
+++ src/Products/PloneMeeting/migrations/migrate_to_4109.py
@@ -11,11 +11,19 @@ class Migrate_To_4109(Migrator):
     def force_email_from_address(self):
         logger.info("Fixing email from address...")
         mail_panel_adapter = MailControlPanelAdapter(self.portal)
-        public_url = get_public_url(self.portal)
-        mail_address = public_url.replace("https://", "")
-        index = mail_address.index("-pm")
-        mail_address = "%s-delib@imio.be" % mail_address[:index]
-        mail_panel_adapter.set_email_from_address(mail_address)
+        smtp_host = mail_panel_adapter.smtp_host or u''
+        if smtp_host.strip() != u'localhost':
+            logger.info("Bypassing, smtp_host is not localhost...")
+        else:
+            public_url = get_public_url(self.portal)
+            mail_address = public_url.replace("https://", "")
+            if "-pm" not in mail_address:
+                logger.info("Bypassing, \"-pm\" not found in public_url...")
+            else:
+                index = mail_address.index("-pm")
+                mail_address = "%s-delib@imio.be" % mail_address[:index]
+                logger.info("Setting \"{0}\" as email_from_address.".format(mail_address))
+                mail_panel_adapter.set_email_from_address(mail_address)
         logger.info('Done.')
 
     def run(self, from_migration_to_41=False):
