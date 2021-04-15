27273259230c61a1e4a8478bdea39d6b3abb2b8c
Gauthier Bastien <g.bastien@imio.be>
2021-04-15 12:32:18 +0200

    When CORS problem happen with CKeditor imagerotate plugin, display a clear translated message

--- src/Products/PloneMeeting/ckeditor/imagerotate/lang/fr.js
+++ src/Products/PloneMeeting/ckeditor/imagerotate/lang/fr.js
@@ -1,4 +1,7 @@
 CKEDITOR.plugins.setLang('imagerotate', 'fr', {
   rotateRight: 'Rotation horaire',
-  rotateLeft: 'Rotation antihoraire'
+  rotateLeft: 'Rotation antihoraire',
+  errorNoImage: "no image element?",
+  errorNoDOMImage: "no DOM image element?",
+  errorImageFromOtherDomain: "L'image provient d'un autre site et pour des raisons de sécurité, ne peut pas être manipulée avant d'avoir été sauvegardée une première fois.  Sauvegardez et éditez à nouveau",
 });
