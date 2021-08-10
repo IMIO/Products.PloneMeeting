6432817c23bb034768a4a9ff47974025d9e16a56
Gauthier Bastien <gauthier.bastien@imio.be>
2021-08-10 16:28:31 +0200

    Fixed call to listStates in MeetingConfig.getMeetingStatesAcceptingItemsForMeetingManagers

--- src/Products/PloneMeeting/MeetingConfig.py
+++ src/Products/PloneMeeting/MeetingConfig.py
@@ -7142,7 +7142,7 @@ class MeetingConfig(OrderedBaseFolder, BrowserDefaultMixin):
 
     def getMeetingStatesAcceptingItemsForMeetingManagers(self):
         '''See doc in interfaces.py.'''
-        return tuple([state for state in self.adapted().listStates("Meeting") if state != 'closed'])
+        return self.listStates("Meeting", excepted='closed', with_state_title=False)
 
     def getMeetingsAcceptingItems_cachekey(method, self, review_states=('created', 'frozen'), inTheFuture=False):
         '''cachekey method for self.getMeetingsAcceptingItems.'''
