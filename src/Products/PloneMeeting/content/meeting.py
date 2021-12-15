dc72f29e1050543580e9849251ab4f47020da860
Antoine Duchêne <antoine.duchene@imio.be>
2021-12-15 10:58:50 +0100

    PM-3797 Meeting videoconference (#138)
    Meeting videoconference functionality

--- src/Products/PloneMeeting/content/meeting.py
+++ src/Products/PloneMeeting/content/meeting.py
@@ -249,6 +249,12 @@ class IMeeting(IDXMeetingContent):
         default=False,
         required=False)
 
+    form.widget('videoconference', RadioFieldWidget)
+    videoconference = schema.Bool(
+        title=_(u'title_videoconference'),
+        default=False,
+        required=False)
+
     form.widget('assembly', PMTextAreaFieldWidget)
     assembly = RichText(
         title=_(u"title_assembly"),
@@ -424,7 +430,7 @@ class IMeeting(IDXMeetingContent):
                    fields=['date', 'start_date', 'mid_date', 'end_date',
                            'approval_date', 'convocation_date',
                            'validation_deadline', 'freeze_deadline',
-                           'place', 'place_other',
+                           'place', 'place_other', 'videoconference',
                            'pre_meeting_date', 'pre_meeting_place',
                            'extraordinary_session'])
 
@@ -800,6 +806,9 @@ class Meeting(Container):
             {'optional': False,
              'condition': "python:view.show_field('place') and "
                 "(view.mode != 'display' or context.place == u'other')"},
+        'videoconference':
+            {'optional': True,
+             'condition': ""},
         'extraordinary_session':
             {'optional': True,
              'condition': ""},
