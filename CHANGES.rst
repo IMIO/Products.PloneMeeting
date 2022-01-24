22be1c964ecd7397295ac53bb7a48cc3755dc826
Gauthier Bastien <gauthier.bastien@imio.be>
2022-01-24 08:57:18 +0100

    Back to development: 4.2rc5

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,12 @@ Changelog
 =========
 
 
+4.2rc5 (unreleased)
+-------------------
+
+- Nothing changed yet.
+
+
 4.2rc4 (2022-01-24)
 -------------------
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '4.2rc4'
+version = '4.2rc5.dev0'
 
 setup(name='Products.PloneMeeting',
       version=version,
