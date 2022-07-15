04ce89e8eb39eb4a0cbf24ab32c259e2ca1dfbbd
Gauthier Bastien <gauthier.bastien@imio.be>
2022-07-15 15:08:37 +0200

    Added possibility to add images to `MeetingItemTemplate/MeetingItemReccuring`. Display the `folder_contents` tab on items of the `MeetingConfig`. See #PM-3935

--- src/Products/PloneMeeting/migrations/migrate_to_4204.py
+++ src/Products/PloneMeeting/migrations/migrate_to_4204.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 
+from imio.helpers.setup import load_type_from_package
 from Products.PloneMeeting.migrations import logger
 from Products.PloneMeeting.migrations import Migrator
 from Products.PloneMeeting.setuphandlers import _configurePortalRepository
 
 
 class Migrate_To_4204(Migrator):
+
+    def _reloadItemTemplateAndRecurringTypes(self):
+        """Reload MeetingItemTemplate/MeetingItemRecurring portal_types to fix
+           the allowed_content_types to only accept "Image"."""
+        logger.info('Reloading MeetingItemTemplate/MeetingItemRecurring portal_types...')
+        # first update MeetingItemTemplate/MeetingItemRecurring base portal_types
+        load_type_from_package('MeetingItemTemplate', 'Products.PloneMeeting:default')
+        load_type_from_package('MeetingItemRecurring', 'Products.PloneMeeting:default')
+        for cfg in self.tool.objectValues('MeetingConfig'):
+            cfg.at_post_edit_script()
+        logger.info('Done.')
 
     def run(self, extra_omitted=[], from_migration_to_4200=False):
 
@@ -14,13 +26,15 @@ class Migrate_To_4204(Migrator):
         # not necessary if executing the full upgrade to 4200
         if not from_migration_to_4200:
             _configurePortalRepository()
+            self._reloadItemTemplateAndRecurringTypes()
         logger.info('Done.')
 
 
 def migrate(context):
     '''This migration function will:
 
-       1) Configure portal_repository.
+       1) Configure portal_repository;
+       2) Reload MeetingItemTemplate/Recurring portal_types.
     '''
     migrator = Migrate_To_4204(context)
     migrator.run()
