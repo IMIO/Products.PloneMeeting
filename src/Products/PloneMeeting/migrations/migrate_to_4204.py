241846b286da9a05d4f19d5dd0c5a97f7c3708ee
Gauthier Bastien <gauthier.bastien@imio.be>
2022-09-30 17:08:15 +0200

    Fixed typo

--- src/Products/PloneMeeting/migrations/migrate_to_4204.py
+++ src/Products/PloneMeeting/migrations/migrate_to_4204.py
@@ -38,7 +38,7 @@ class Migrate_To_4204(Migrator):
         """Boolean field yearlyInitMeetingNumber is not a list of choices called
            yearlyInitMeetingNumbers."""
         logger.info('Migrating field "yearlyInitMeetingNumber" to '
-                    '"yearlyInitMeetingNumbers" for every meetings...')
+                    '"yearlyInitMeetingNumbers" for every MeetingConfigs...')
         for cfg in self.tool.objectValues('MeetingConfig'):
             if hasattr(cfg, "yearlyInitMeetingNumber"):
                 if cfg.yearlyInitMeetingNumber is True:
