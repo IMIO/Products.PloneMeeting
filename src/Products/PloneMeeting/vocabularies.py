cf10303724ce4e8c7bac70e6a98c5079edd48bf8
Gauthier Bastien <gauthier.bastien@imio.be>
2023-07-06 14:46:17 +0200

    Pm 4089 change docs imio url (#239)
    * Updated link to the documentation.
    See #PM-4089
    
    * Updated code, moved actions.xml update from 4207 to 4208

--- src/Products/PloneMeeting/vocabularies.py
+++ src/Products/PloneMeeting/vocabularies.py
@@ -1637,13 +1637,17 @@ class PMPortalTypesVocabulary(PortalTypesVocabularyFactory):
         if cfg:
             # available for item, meeting and advice
             itemTypeName = cfg.getItemTypeName()
-            res.append(SimpleTerm(itemTypeName, itemTypeName, translate(itemTypeName,
-                                                                        domain="plone",
-                                                                        context=context.REQUEST)))
+            res.append(SimpleTerm(itemTypeName,
+                                  itemTypeName,
+                                  translate(itemTypeName,
+                                            domain="plone",
+                                            context=context.REQUEST)))
             meetingTypeName = cfg.getMeetingTypeName()
-            res.append(SimpleTerm(meetingTypeName, meetingTypeName, translate(meetingTypeName,
-                                                                              domain="plone",
-                                                                              context=context.REQUEST)))
+            res.append(SimpleTerm(meetingTypeName,
+                                  meetingTypeName,
+                                  translate(meetingTypeName,
+                                            domain="plone",
+                                            context=context.REQUEST)))
             # manage multiple 'meetingadvice' portal_types
             for portal_type in tool.getAdvicePortalTypes():
                 res.append(SimpleTerm(portal_type.id,
@@ -1671,7 +1675,8 @@ class PMExistingPODTemplate(ExistingPODTemplateFactory):
            This could be a template stored in "contacts" or somewhere else."""
         template = brain.getObject()
         if template.aq_inner.aq_parent.id == "podtemplates":
-            parent_title = template.aq_inner.aq_parent.aq_parent.Title(include_config_group=True)
+            parent_title = template.aq_inner.aq_parent.aq_parent.Title(
+                include_config_group=True)
         else:
             parent_title = template.aq_inner.aq_parent.Title()
         return u'{} ➔ {} ➔ {}'.format(
