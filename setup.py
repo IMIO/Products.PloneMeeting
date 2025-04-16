2a72c754ac84a07789ee22c8ecdc8d588e5d48d5
Gauthier Bastien <gauthier.bastien@imio.be>
2025-04-16 15:14:55 +0200

    Back to development: 4.2.20rc2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '4.2.20rc1'
+version = '4.2.20rc2.dev0'
 
 setup(name='Products.PloneMeeting',
       version=version,
